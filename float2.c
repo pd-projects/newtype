74c68f0ac489bd54f871adc9706b86a563292bf5
Hans-Christoph Steiner <hans@eds.org>
2011-09-09 13:23:53 -0400

    added full test suite when converting symbol to float

--- float2.c
+++ float2.c
@@ -42,8 +42,54 @@ static void float2_float(t_float2 *x, t_float f)
 
 static void float2_symbol(t_float2 *x, t_symbol *s)
 {
-    x->x_f = atof(s->s_name);
-    outlet_float(x->x_obj.ob_outlet, x->x_f);
+    int isfloat = 1;
+    int len = strlen(s->s_name);
+    char buf[MAXPDSTRING];
+    t_float f = atof(s->s_name);
+    if (len == 0) {
+        isfloat = 0;
+        sprintf(buf, "zero length symbol");
+    } else if(s->s_name[0] == '+') {
+        /* pd doesn't consider things starting with '+' as float */
+        isfloat = 0;
+        sprintf(buf, "starts with +");
+    } else if(s->s_name[0] == '0' && s->s_name[1] == 'x') {
+        /* pd doesn't support hex 0x format numbers as floats */
+        isfloat = 0;
+        sprintf(buf, "0x hex not supported");
+    } else if (f == 0) {
+        if(len == 1 && s->s_name[0] != '0') {
+            isfloat = 0;
+            sprintf(buf, "length == 1 and != '0'");
+        } else if (len > 1) {
+            if((s->s_name[0] != '0' && s->s_name[0] != '.' && s->s_name[0] != '-') && 
+               s->s_name[1] != '0') {
+                isfloat = 0;
+                sprintf(buf, "length > 1 but doesn't start with '.0' or '-0'");
+            } else {
+                /* we already checked positions 0 and 1 so skip */
+                char* str = &s->s_name[2];
+                int founddot = 0;
+                while (*str != '\0') {
+                    if(*str == '.')
+                        founddot++; 
+                    if((*str != '0' && *str != '.') || founddot) {
+                        isfloat = 0;
+                        sprintf(buf, "== 0 and found characters besides '0' or '.'");
+                        break;
+                    }
+                    str++;
+                }
+            }
+        }
+    }
+    if(isfloat)
+    {
+        x->x_f = f;
+        outlet_float(x->x_obj.ob_outlet, x->x_f);
+    }
+    else
+        pd_error(x,"cannot convert '%s' to a float!\n\t(%s)", s->s_name, buf);
 }
 
 void float2_setup(void)
